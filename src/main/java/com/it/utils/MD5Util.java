3c66c4014f355cfcdc60048b973d2d82c459b4cc
“xw8209” <820928313@qq.com>
2022-11-18 23:37:20 +0800

    增加2次MD5加密

---
+++ src/main/java/com/it/utils/MD5Util.java
@@ -0,0 +1,45 @@
+package com.it.utils;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class MD5Util {
+
+    public static String md5(String src){
+        return DigestUtils.md5Hex(src);
+    }
+
+    //和前端的盐统一，第一次加密在前端
+    private static final String salt="1a2b3c4d";
+
+
+    /**
+     * 前端传，后端接受的密码
+     * @return
+     */
+    public static String inputPassToFormPass(String inputPass){
+        String str = salt.charAt(2)+inputPass+salt.charAt(5)+salt.charAt(4);
+        return md5(str);
+    }
+
+    /**
+     * 后端密码到数据库接受的密码,二次加密
+     */
+    public static String formPassToDBPass(String formPass,String salt){
+        String str = salt.charAt(2)+formPass+salt.charAt(5)+salt.charAt(4);
+        return md5(str);
+    }
+
+    /**
+     * 后端真正处理的方法
+     * @param inputPass
+     * @param salt
+     * @return
+     */
+    public static String inputPassToDBPass(String inputPass,String salt){
+        String formPass = inputPassToFormPass(inputPass);
+        String dbPass = formPassToDBPass(formPass, salt);
+        return dbPass;
+    }
+
+
+}
