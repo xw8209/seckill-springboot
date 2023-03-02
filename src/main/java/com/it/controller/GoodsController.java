7b47d5df356e00a3173012891836b00d557c7fcd
“xw8209” <820928313@qq.com>
2023-03-02 22:42:42 +0800

    1.添加了商品列表和商品详情的缓存

--- src/main/java/com/it/controller/GoodsController.java
+++ src/main/java/com/it/controller/GoodsController.java
@@ -13,11 +13,14 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 @RequestMapping("/goods")
@@ -29,6 +32,10 @@ private IGoodsService goodsService;
 
 @Autowired
 private RedisTemplate redisTemplate;
+
+@Autowired
+private ThymeleafViewResolver viewResolver;
+
     /**
      * windows 优化前 562
      * @param model
@@ -36,20 +43,29 @@ private RedisTemplate redisTemplate;
      * @return
      */
     @RequestMapping(value = "/toList",produces = "text/html;charset = utf-8")
-    public String toList(Model model, User user){
+    public String toList(Model model, User user, HttpServletRequest request, HttpServletResponse response){
         //Redis中获取页面
         ValueOperations valueOperations = redisTemplate.opsForValue();
         String html = (String) valueOperations.get("goodsList");
+        //如果不为空直接返回
         if(!StringUtils.isEmpty(html)){
+            valueOperations.set("goodsList", html, 60, TimeUnit.SECONDS);
             return html;
         }
+        //否则手动渲染
 //        if(user == null){
 //            return "login";
 //        }
         model.addAttribute("user",user);
         model.addAttribute("goodsList",goodsService.findGoodVo());
         //return "goodsList";
-        return null;
+        WebContext webContext = new WebContext(request, response, request.getServletContext(), request.getLocale(),model.asMap());
+        String newHtml = viewResolver.getTemplateEngine().process("goodsList", webContext);
+        if(!StringUtils.isEmpty(newHtml)) {
+            valueOperations.set("goodsList", newHtml, 60, TimeUnit.SECONDS);
+        }
+        return newHtml;
+
     }
 
     /**
@@ -58,8 +74,14 @@ private RedisTemplate redisTemplate;
      * @param user
      * @return
      */
-    @RequestMapping("/toDetail/{goodsId}")
-    public String toDetail(Model model, User user, @PathVariable Long goodsId){
+    @RequestMapping(value ="/toDetail/{goodsId}", produces = "text/html;charset=utf-8")
+    public String toDetail(Model model, User user, @PathVariable Long goodsId, HttpServletRequest request, HttpServletResponse response){
+        //redis中获取页面
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        String html = (String) valueOperations.get("goodsDetail:" + goodsId);
+        if(!StringUtils.isEmpty(html)) {
+            return html;
+        }
         model.addAttribute("user",user);
         GoodsVo goodsVo = goodsService.findGoodVoByGoodsId(goodsId);
         Date startDate = goodsVo.getStartDate();
@@ -84,6 +106,11 @@ private RedisTemplate redisTemplate;
         model.addAttribute("goods",goodsVo);
         model.addAttribute("secKillStatus",secKillStatus);
         model.addAttribute("remainSeconds",remainSeconds);
-        return "goodsDetail";
+        WebContext webContext = new WebContext(request, response, request.getServletContext(), request.getLocale(),model.asMap());
+        String newHtml = viewResolver.getTemplateEngine().process("goodsDetail", webContext);
+        if(!StringUtils.isEmpty(newHtml)) {
+            valueOperations.set("goodsDetail:"+goodsId, newHtml, 60, TimeUnit.SECONDS);
+        }
+        return newHtml;
     }
 }
